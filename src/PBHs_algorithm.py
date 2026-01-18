a1ff63d0e20e63566bcb8bc1ef16d7ff2ad63a38
Rodrigo de Souza <rsouza01@gmail.com>
2026-01-19 00:25:18 +0100

    WIP

--- src/PBHs_algorithm.py
+++ src/PBHs_algorithm.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3  
 
-import math 
+import math
+import json
 import pandas as pd
 import numpy as np
 from scipy import constants
-from scipy.integrate import solve_ivp
+from pathlib import Path
 
-# Constants
-M_CRITICAL = 1e20  # Critical mass in grams
-MINIMUM_SURVIVING_MASS = 5e14     # Minimum mass in grams
-SIGMA_DIFF = 0.5
+M_CRITICAL = 0
+MINIMUM_SURVIVING_MASS = 0
+SIGMA_DIFF = 0
 
 def phi(mass: float) -> float:
     """
@@ -24,15 +24,25 @@ def evaporation_time(mass: float) -> float:
     """
     return 1e10 * (mass/MINIMUM_SURVIVING_MASS) ** 3  # in years
 
+def load_config(config_path):
+    # Load constants from file
+    with open(config_path, 'r', encoding='utf-8') as f:
+        _constants = json.load(f)
+    globals().update(
+        M_CRITICAL = _constants["M_CRITICAL"],
+        MINIMUM_SURVIVING_MASS = _constants["MINIMUM_SURVIVING_MASS"],
+        SIGMA_DIFF = _constants["SIGMA_DIFF"]
+    )
 
 def main() -> None:
     """
     Entry point of the program.
     """
+    # load_config(Path(__file__).parent / "constants.json")
+    load_config("./constants.json")
     m = 1e19
     print(f"Phi({m} g) = {phi(m):.2e} g-1")
     print(f"Evaporation time for {m} g = {evaporation_time(m)/1e9:.2e} billion years")
 
 if __name__ == "__main__":
-
     main()
